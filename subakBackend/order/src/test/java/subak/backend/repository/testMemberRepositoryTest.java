b3af315df29e059689426a5d9a6ffbb50b5dfa1d
Seong_min <yun2044992@naver.com>
2023-11-08 13:59:35 +0900

    Delete : 테스트파일 삭제

--- subakBackend/order/src/test/java/subak/backend/repository/testMemberRepositoryTest.java
+++ subakBackend/order/src/test/java/subak/backend/repository/testMemberRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import subak.backend.domain.TestMember;
 
 @SpringBootTest
 class TestMemberRepositoryTest {
