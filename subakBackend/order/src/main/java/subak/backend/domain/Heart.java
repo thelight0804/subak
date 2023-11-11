9046989db545fba706df7710a3788fba2fab54da
Seong_min <yun2044992@naver.com>
2023-11-11 20:18:59 +0900

    Add: 도메인 추가

---
+++ subakBackend/order/src/main/java/subak/backend/domain/Heart.java
@@ -0,0 +1,26 @@
+package subak.backend.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+public class Heart {
+
+    @Id @GeneratedValue
+    @Column(name = "heart_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
