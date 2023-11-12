1ac774f12ed1afc51db88b5e1e66109c51b4c3ae
Seong_min <yun2044992@naver.com>
2023-11-12 22:28:24 +0900

    Add: 회원 아이디(이메일)찾기, 비밀번호 찾기 및 재설정 테스트

--- subakBackend/order/src/test/java/subak/backend/service/MemberServiceTest.java
+++ subakBackend/order/src/test/java/subak/backend/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 import subak.backend.domain.Member;
 import subak.backend.repository.MemberRepository;
@@ -11,6 +12,8 @@ import subak.backend.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,9 +25,6 @@ class MemberServiceTest {
     @Autowired
     MemberService memberService;
 
-    @Autowired
-    EntityManager em;
-
 
     @Test
     @Transactional
@@ -32,16 +32,15 @@ class MemberServiceTest {
     void 회원가입() throws Exception { // jUnit5부터는 public을 붙이지 않아도 된다.
 
         Member member = new Member();
-        member.setEmail("0003@gmail.com");
-        member.setName("0");
-        member.setPassword("0");
-        member.setPhone("01000000000");
 
+        member.setEmail("0004@gmail.com");
+        member.setPassword("0");
+        member.setName("0");
+        member.setPhone("01000000000");
         Long savedMember = memberService.join(member);
 
         Assertions.assertEquals(member, memberRepository.findOne(savedMember));
     }
-
 
 
     @Test // jUnit5에서는 @Test에 expected 속성이 지원되지 않는다.
@@ -50,13 +49,70 @@ class MemberServiceTest {
     void 중복회원검증() throws Exception {
 
         Member member1 = new Member();
+
         member1.setEmail("0004@gmail.com");
+        member1.setPassword("0");
+        member1.setName("0");
+        member1.setPhone("01000000000");
         memberService.join(member1);
 
         Member member2 = new Member();
         member2.setEmail("0004@gmail.com");
 
-        assertThrows(IllegalStateException.class, () -> {memberService.join(member2);});
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void 이메일찾기() throws Exception {
+        Member member = createMember("0004@gmail.com", "0", "0", "01000000000");
+
+        String findEmail = memberService.findMemberEmail(member.getName(), member.getPhone());
+        assertEquals("0004@gmail.com", findEmail);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void 이메일찾기_일치하는회원없음_예외() throws Exception {
+        Member member = createMember("0004@gmail.com", "0", "0", "01000000000");
+
+        //name과 phone을 다른 정보로 입력했을 때, 예외가 발생하는지
+        assertThrows(IllegalArgumentException.class,
+                () -> memberService.findMemberEmail("nonUser", "01012345678"));
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void 비밀번호찾기_일치하는회원() throws Exception {
+        Member member = createMember("0004@gmail.com", "0", "0", "01000000000");
+
+        String newPassword = "newPassword";
+        memberService.findPassword(member.getEmail(), member.getName(), member.getPhone(), newPassword);
+
+        // 업데이트된 비밀번호로 로그인이 가능한지 확인
+        List<Member> updatedMembers = memberRepository.findByEmail(member.getEmail());
+        assertFalse(updatedMembers.isEmpty());
+
+        // BCryptPasswordEncoder를 사용하여 암호화된 비밀번호를 확인
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        assertTrue(passwordEncoder.matches(newPassword, updatedMembers.get(0).getPassword()));
+    }
+
+    private Member createMember(String email, String name, String password, String phone) {
+        Member member = new Member();
+        member.setEmail(email);
+        member.setName(name);
+        member.setPassword(password);
+        member.setPhone(phone);
+
+        memberRepository.save(member);
+        return member;
     }
 
 
