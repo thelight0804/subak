0cf1cb29495d7426ded062abcf37ba90866a9e80
Seong_min <yun2044992@naver.com>
2023-11-24 23:48:58 +0900

    Update: @AllArgsConstructor 추가

--- subakBackend/order/src/main/java/subak/backend/dto/response/member/JoinResponse.java
+++ subakBackend/order/src/main/java/subak/backend/dto/response/member/JoinResponse.java
@@ -1,15 +1,13 @@
 package subak.backend.dto.response.member;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 @Data
+@AllArgsConstructor
 public class JoinResponse {
     private final Long id;
     private HttpStatus httpStatus;
 
-    public JoinResponse(Long id, HttpStatus httpStatus) {
-        this.id = id;
-        this.httpStatus = httpStatus;
-    }
 }
