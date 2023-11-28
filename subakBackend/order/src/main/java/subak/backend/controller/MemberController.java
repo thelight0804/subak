d27c86a65e0e9fe9fa32085476678bef680fc88e
Seong_min <yun2044992@naver.com>
2023-11-28 23:49:06 +0900

    Add: 회원 정보 수정 기능 추가(이름, 프로필사진)

--- subakBackend/order/src/main/java/subak/backend/controller/MemberController.java
+++ subakBackend/order/src/main/java/subak/backend/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import subak.backend.domain.Member;
 import subak.backend.dto.request.member.FindMemberEmailRequest;
 import subak.backend.dto.request.member.JoinRequest;
@@ -70,6 +71,17 @@ public class MemberController {
                 request.getNewPassword());
         return ResponseEntity.ok(result);
     }
+
+    @ApiOperation(value = "회원 정보 수정", notes = "회원 아이디를 통해 이름과 프로필을 수정한다.")
+    @PutMapping("/user/{userId}")
+    public ResponseEntity<String> updateMember(@PathVariable Long userId,
+                                               @RequestParam String name,
+                                               @RequestParam MultipartFile file) {
+
+        memberService.updateMember(userId, name, file);
+        return ResponseEntity.ok("MemberUpdate successful");
+    }
+
 
     private Member mapToMember(JoinRequest request) {
         Member member = new Member();
--- subakBackend/order/src/test/java/subak/backend/service/MemberServiceTest.java
+++ subakBackend/order/src/test/java/subak/backend/service/MemberServiceTest.java
@@ -3,6 +3,8 @@ package subak.backend.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
@@ -16,6 +18,7 @@ import javax.transaction.Transactional;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Transactional
@@ -31,31 +34,28 @@ class MemberServiceTest {
 
     @Test
     @Rollback(value = true)
-    void 회원가입() throws Exception { // jUnit5부터는 public을 붙이지 않아도 된다.
-
-        Member member = new Member();
-
-        member.setEmail("0004@gmail.com");
-        member.setPassword("0");
-        member.setName("0");
-        member.setPhone("01000000000");
+    void 회원가입() throws Exception {
+        // given
+        Member member = createMember("0004@gmail.com", "0", "0", "01000000000");
         Long savedMemberId = memberService.join(member);
 
+        // then
         Optional<Member> savedMember = memberRepository.findById(savedMemberId);
         assertTrue(savedMember.isPresent());
     }
 
 
-    @Test // jUnit5에서는 @Test에 expected 속성이 지원되지 않는다.
+    @Test
     @Rollback
     void 중복회원검증() throws Exception {
-
+        // given
         Member member1 = createMember("0004@gmail.com", "0", "0", "01000000000");
         memberService.join(member1);
 
-        Member member2 = new Member();
-        member2.setEmail("0004@gmail.com");
+        // when
+        Member member2 = createMember("0004@gmail.com", "0", "0", "01000000000");
 
+        // then
         assertThrows(MemberException.DuplicateMemberException.class, () -> {
             memberService.join(member2);
         });
@@ -132,29 +132,45 @@ class MemberServiceTest {
 
     @Test
     @Rollback
-    void 회원탈퇴() throws Exception{
-        // Given
-        String email = "test5@gmail.com";
-        String password = "password123";
-        String name = "TestUser5";
-        String phone = "01012345678";
-
-        Member member = new Member();
-        member.setEmail(email);
-        member.setPassword(password);
-        member.setName(name);
-        member.setPhone(phone);
-
+    void 회원탈퇴() throws Exception {
+        // given
+        Member member = createMember("test5@gmail.com", "password123", "TestUser5", "01012345678");
         memberService.join(member);
 
-        // When
-        memberService.withdraw(email);
+        // when
+        memberService.withdraw(member.getEmail());
 
-        // Then
-        Optional<Member> withdrawnMemberOptional = memberRepository.findByEmail(email);
-        assertTrue(withdrawnMemberOptional.isPresent(), "탈퇴한 회원 정보를 찾을 수 없습니다.");
-        assertEquals(MemberStatus.DELETE, withdrawnMemberOptional.get().getStatus(), "회원 상태가 탈퇴 상태가 아닙니다.");
+        // then
+        Optional<Member> withdrawnMemberOptional = memberRepository.findByEmail(member.getEmail());
+        assertTrue(withdrawnMemberOptional.isPresent());
+        assertEquals(MemberStatus.DELETE, withdrawnMemberOptional.get().getStatus());
     }
+
+    @Test
+    @Rollback
+    public void 회원정보수정() throws Exception {
+        // given
+        String email = "test1@gmail.com";
+        String oldName = "Old Name";
+        String password = "password123";
+        String phone = "01012345678";
+
+        Member member = createMember(email, oldName, password, phone);
+        memberService.join(member);
+
+        String newName = "New Name";
+        MockMultipartFile newFile = new MockMultipartFile("file", "hello.png", MediaType.IMAGE_PNG_VALUE, "Hello, World!".getBytes());
+
+        // when
+        memberService.updateMember(member.getId(), newName, newFile);
+
+        // then
+        Optional<Member> updatedMemberOptional = memberRepository.findById(member.getId());
+        assertTrue(updatedMemberOptional.isPresent());
+        assertEquals(newName, updatedMemberOptional.get().getName());
+        assertArrayEquals(newFile.getBytes(), updatedMemberOptional.get().getPicture());
+    }
+
 
 
     private Member createMember(String email, String name, String password, String phone) {
