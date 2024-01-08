bd485b4a168482d6cdcfc799579bba44bac8f5e3
Seong_min <yun2044992@naver.com>
2024-01-08 21:21:13 +0900

    Update: 게시물 카테고리 String 타입 -> Enum 변경

---
+++ subakBackend/order/src/main/java/subak/backend/domain/enumType/Category.java
@@ -0,0 +1,15 @@
+package subak.backend.domain.enumType;
+
+/**
+ * 카테고리 타입
+ * [디지털/가전, 가구/인테리어, 의류, 도서/티켓/음반/게임, 뷰티/미용, 기타]
+ */
+
+public enum Category {
+
+    ELECTRONICS, // 디지털,가전
+    FURNITURE, // 가구/인테리어
+    CLOTHING, // 의류
+    BOOKS_TICKETS_RECORDS_GAMES, //도서/티켓/음반/게임
+    BEAUTY, // 기타 등등
+}
