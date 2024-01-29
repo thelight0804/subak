29bcd9b960860c245cf0b4f0e18527d45f8c4f28
thelight0804 <thelight0804@gmail.com>
2024-01-29 17:12:10 +0900

    Add: 판매내역 헤더
    판매내역 페이지의 헤더 구현

--- subakFront/src/styles/itemHistory/itemHistoryList.js
+++ subakFront/src/styles/itemHistory/itemHistoryList.js
@@ -1,9 +1,19 @@
 import {StyleSheet} from "react-native";
+import { colorPalette } from "../shared";
 
 const styles = StyleSheet.create({
-  footer :{
-    padding: 0,
-  }
+  header: {
+    color: colorPalette.white,
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  profileImage: {
+    width: 70,
+    height: 70,
+    marginRight: 10,
+    borderRadius: 50,
+  },
 })
 
 export default styles;
